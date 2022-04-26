764f5a7a630ec67158aa66ab6d47aaf21e226dc6
shyam0205 <vigneshshyam8@gmail.com>
2022-04-26 14:08:37 +0530

    program

---
+++ Add.java
@@ -0,0 +1,16 @@
+package org.maths;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// sum of two numbers
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println(c);
+		System.out.println("im a super hero");
+		System.out.println("im shyam supreme hero");
+		System.out.println("employee");
+	}
+
+}
